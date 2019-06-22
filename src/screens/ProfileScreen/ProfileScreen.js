e1d1f4fe13533922f896b26eedff6b8324415148
Hadi Shad <hadi@mrhadi.com>
2019-06-22 15:28:21 +1000

    Nickname box added

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -12,14 +12,22 @@ import {
   TouchableOpacity,
   Modal,
   View,
-  Image
+  Image,
+  Text,
+  TextInput
 } from 'react-native'
 import IcoMoon from '../../../icomoon/IcoMoon'
 import bg from '../../assets/images/profile/bg.png'
 import Colors from '../../styles/colors'
-import { fontScale, scaleWidth, scaleY } from '../../utils/scaleUtils'
+import {
+  fontScale,
+  scaleHeight,
+  scaleWidth,
+  scaleY
+} from '../../utils/scaleUtils'
 import Popup from '../../components/Popup/Popup'
 import AvatarBrowser from '../../components/AvatarBrowser/AvatarBrowser'
+import EvilIcons from 'react-native-vector-icons/EvilIcons'
 
 export default class ProfileScreen extends Component {
   static propTypes = {
@@ -49,49 +57,79 @@ export default class ProfileScreen extends Component {
     this.hideAvatarBrowser()
   }
 
+  renderProfilePhoto = avatar => (
+    <View style={styles.profilePhoto}>
+      <TouchableOpacity
+        style={styles.addPhoto}
+        onPress={this.showAvatarBrowser}
+      >
+        {avatar === '' ? (
+          <IcoMoon
+            name="add-profile-photo"
+            size={fontScale(24)}
+            color="white"
+          />
+        ) : (
+          <View style={styles.avatarContainer}>
+            <Image
+              style={styles.userAvatar}
+              source={{ uri: `${avatar.name}` }}
+            />
+          </View>
+        )}
+      </TouchableOpacity>
+      <Text style={styles.addPhotoText}>
+        {avatar === '' ? 'Add profile photo' : 'Edit profile photo'}
+      </Text>
+    </View>
+  )
+
+  renderAvatarBrowser = modalVisible => (
+    <Modal
+      animationType="fade"
+      transparent
+      visible={modalVisible}
+      onRequestClose={() => {}}
+    >
+      <View style={styles.modalContainer}>
+        <Popup
+          style={styles.popupView}
+          content={
+            <AvatarBrowser
+              onSelected={avatar => this.handleAvatarSelected(avatar)}
+            />
+          }
+          returnButtonTitle="Return"
+          onReturnClick={this.hideAvatarBrowser}
+        />
+      </View>
+    </Modal>
+  )
+
+  renderNickName = () => (
+    <View style={styles.nickNameContainer}>
+      <EvilIcons style={{ color: Colors.textViolet }} name="user" size={26} />
+      <TextInput
+        onChangeText={() => {}}
+        style={styles.nickNameTextInput}
+        placeholder="Add your nickname"
+        returnKeyLabel="Done"
+        returnKeyType="go"
+        spellCheck={false}
+        autoCapitalize="none"
+        maxLength={20}
+      />
+    </View>
+  )
+
   render() {
     const { modalVisible, userAvatar } = this.state
     return (
       <ImageBackground style={styles.bgImage} source={bg}>
         <SafeAreaView style={styles.container}>
-          <TouchableOpacity
-            style={styles.addPhoto}
-            onPress={this.showAvatarBrowser}
-          >
-            {userAvatar === '' ? (
-              <IcoMoon
-                name="add-profile-photo"
-                size={fontScale(24)}
-                color="white"
-              />
-            ) : (
-              <View style={styles.avatarContainer}>
-                <Image
-                  style={styles.userAvatar}
-                  source={{ uri: `${userAvatar.name}` }}
-                />
-              </View>
-            )}
-          </TouchableOpacity>
-          <Modal
-            animationType="fade"
-            transparent
-            visible={modalVisible}
-            onRequestClose={() => {}}
-          >
-            <View style={styles.modalContainer}>
-              <Popup
-                style={styles.popupView}
-                content={
-                  <AvatarBrowser
-                    onSelected={avatar => this.handleAvatarSelected(avatar)}
-                  />
-                }
-                returnButtonTitle="Return"
-                onReturnClick={this.hideAvatarBrowser}
-              />
-            </View>
-          </Modal>
+          {this.renderProfilePhoto(userAvatar)}
+          {modalVisible && this.renderAvatarBrowser(modalVisible)}
+          {this.renderNickName()}
         </SafeAreaView>
       </ImageBackground>
     )
@@ -141,5 +179,38 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  profilePhoto: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  addPhotoText: {
+    marginTop: scaleHeight(10),
+    fontSize: fontScale(12),
+    color: Colors.textViolet
+  },
+  nickNameContainer: {
+    flexDirection: 'row',
+    marginTop: scaleHeight(40),
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    width: scaleWidth(220),
+    height: scaleWidth(50),
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.15,
+    shadowRadius: 8,
+    elevation: 5,
+    alignItems: 'center',
+    padding: 10
+  },
+  nickNameTextInput: {
+    flex: 1,
+    height: scaleWidth(40),
+    fontSize: fontScale(14),
+    color: Colors.textViolet,
+    marginLeft: scaleWidth(10)
   }
 })
