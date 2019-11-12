0e1298a1fcb05e436991beaff4070dba88fd5d8d
Hadi Shad <hadi@mrhadi.com>
2019-11-12 12:46:12 +1100

    More updates

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { API } from '../constants'
 import { deviceUniqueID } from '../utils/deviceInfo'
-import { bugsnagError } from '../utils/bugsnag'
 
 const instance = axios.create({
   baseURL: API,
@@ -15,7 +14,6 @@ instance.interceptors.request.use(config => config, error => error)
 instance.interceptors.response.use(
   response => response,
   error => {
-    bugsnagError(error)
     if (error.response && error.response.data) {
       console.log('axios:', error.response.data)
     } else {
